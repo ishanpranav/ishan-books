9ce0b4b732270878e0b0a35fa32e917bb54513cb
Ishan Pranav <ishan.pranav@outlook.com>
2023-07-15 19:10:02 -0700

    Drop the old project

--- Liber/Commands/ICommandParent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Liber.Forms;
-using System.Collections.Generic;
-
-namespace Liber.Commands;
-
-internal interface ICommandParent
-{
-    IReadOnlyCollection<Command> GetChildren(MainContext context);
-}
--- Liber/Forms/Companies/EditCompanyForm.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Liber.Forms.Companies;
-
-internal sealed class EditCompanyForm : CompanyForm
-{
-    public EditCompanyForm(MainContext context, Guid _) : base(context.Company) { }
-}
