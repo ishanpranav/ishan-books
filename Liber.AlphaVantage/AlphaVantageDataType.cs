a5f1a42dd643c490c02c181811517986ebf9e2ef
Ishan Pranav <ishan.pranav@outlook.com>
2023-08-17 00:26:37 -0700

    Add AlphaVantage features

---
+++ Liber.AlphaVantage/AlphaVantageDataType.cs
@@ -0,0 +1,55 @@
+﻿// AlphaVantageDataType.cs
+// Copyright (c) 2023 Ishan Pranav. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Liber.AlphaVantage;
+
+public readonly struct AlphaVantageDataType : IEquatable<AlphaVantageDataType>
+{
+    private readonly string _value;
+
+    public static readonly AlphaVantageDataType Json;
+    public static readonly AlphaVantageDataType Csv = new AlphaVantageDataType("csv");
+
+    public AlphaVantageDataType()
+    {
+        _value = "json";
+    }
+
+    private AlphaVantageDataType(string value)
+    {
+        _value = value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is AlphaVantageDataType other && Equals(other);
+    }
+
+    public bool Equals(AlphaVantageDataType other)
+    {
+        return _value == other._value;
+    }
+
+    public static bool operator ==(AlphaVantageDataType left, AlphaVantageDataType right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(AlphaVantageDataType left, AlphaVantageDataType right)
+    {
+        return !(left == right);
+    }
+
+    public override int GetHashCode()
+    {
+        return _value.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return _value;
+    }
+}
---
+++ Liber.AlphaVantage/Progam.cs
@@ -0,0 +1,31 @@
+﻿// Progam.cs
+// Copyright (c) 2023 Ishan Pranav. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using RestSharp.Serializers.CsvHelper;
+
+namespace Liber.AlphaVantage;
+
+internal static class Progam
+{
+    private static async Task Main()
+    {
+        const string apiKey = "";
+        const string symbol = "SWPPX";
+
+        using HttpClient httpClient = new HttpClient();
+        using AlphaVantageClient alphaVantageClient = new AlphaVantageClient(httpClient);
+
+        IReadOnlyCollection<AlphaVantageDailyTimeSeriesResponse>? response = await alphaVantageClient.GetDailyTimeSeriesAsync(apiKey, symbol);
+
+        Console.WriteLine(response);
+
+        CsvHelperSerializer s = new CsvHelperSerializer();
+
+        Console.WriteLine(s.Serialize(response));
+    }
+}
